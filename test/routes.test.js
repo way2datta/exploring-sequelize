c456c5bd623117422dbe23ff912421cd828e2f4a
Dagttatray Kale <way2datta@gmail.com>
2019-07-15 17:44:19 +0530

    Working test

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,43 +1,45 @@
-process.env.NODE_ENV = 'test'
- 
-import chai, { expect } from 'chai'
-import chaiHttp from 'chai-http'
+process.env.NODE_ENV = "test";
 
-import api from '../app'
-const models = require('./../models');
-import seed from './seed'
+import chai, { expect } from "chai";
+import chaiHttp from "chai-http";
 
-chai.use(chaiHttp)
+import api from "../app";
+const models = require("./../models");
+import seed from "./seed";
 
-const app = api(models)
+chai.use(chaiHttp);
 
-describe('API Routes', () => {
-  // start with a fresh DB 
+const app = api(models);
+
+describe("API Routes", () => {
+  // start with a fresh DB
   beforeEach(done => {
-    models.sequelize.sync({ force: true, 
-      //match: /_test$/, 
-    //  logging: false 
-    })
-    .then(() => {
-      return seed(models)
-    }).then(() => {
-      done()
-    })
-
-  })
-  
-  describe('GET /v1/users', (done) => {
-    it('should get a list of users', (done) => {
-      chai.request(app)
-      .get('/api/users')
-      .end((err, res) => {
-        console.log(res.body)
-         expect(res.status).to.equal(200)
-        expect(res).to.be.json
-        // expect(res.body).to.be.a('array')
-        done()
+    models.sequelize
+      .sync({
+        force: true
+        //match: /_test$/,
+        //  logging: false
       })
-    })
-  })
-  
-})
+      .then(() => {
+        return seed(models);
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  describe("GET /v1/users", () => {
+    it("should get a list of users", done => {
+      chai
+        .request(app)
+        .get("/api/users")
+        .end((err, res) => {
+          // console.log(res.body);
+          expect(res.status).to.equal(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a("array");
+          done();
+        });
+    });
+  });
+});
